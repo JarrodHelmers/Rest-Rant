99fdfe6a27de78ff3556c3e7efbf6975e6c8a883
System Administrator <root@Jarrods-MacBook-Air.local>
2022-06-26 20:50:24 -0500

    Added edit form functionality

--- controllers/places.js
+++ controllers/places.js
@@ -1,17 +1,13 @@
 const router = require('express').Router()
 const places = require('../models/places.js')
-
-
-
-router.get('/', (req, res) => {
-  res.render('places/index', {places})
+// GET /places
+ router.get('/', (req, res) => {
+     res.render('places/index', {places})
 })
-
-
 router.post('/', (req, res) => {
-//console.log(req.body)
+  //console.log(req.body)
   if (!req.body.pic) {
-    // place holder image if one is not there
+    // Default image if one is not provided
     req.body.pic = 'http://placekitten.com/400/400'
   }
   if (!req.body.city) {
@@ -23,11 +19,11 @@ router.post('/', (req, res) => {
   places.push(req.body)
   res.redirect('/places')
 })
-
+// NEW
 router.get('/new', (req, res) => {
   res.render('places/new')
 })
-
+// Show route
 router.get('/:id', (req, res) => {
   let id = Number(req.params.id)
   if (isNaN(id)) {
@@ -40,7 +36,7 @@ router.get('/:id', (req, res) => {
     res.render('places/show', { place: places[id], id})
   }
 })
-
+// Edit Route
 router.get('/:id/edit', (req, res) => {
   let id = Number(req.params.id)
   if (isNaN(id)) {
@@ -51,23 +47,50 @@ router.get('/:id/edit', (req, res) => {
   }
   else {
     res.render('places/edit', { place: places[id] })
+    res.render('places/edit', { place: places[id], id:id })
   }
 })
 
+// Put Route
+router.put('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+      res.render('error404')
+  }
+  else if (!places[id]) {
+      res.render('error404')
+  }
+  else {
+      // Dig into req.body and make sure data is valid
+      if (!req.body.pic) {
+          // Default image if one is not provided
+          req.body.pic = 'http://placekitten.com/400/400'
+      }
+      if (!req.body.city) {
+          req.body.city = 'Anytown'
+      }
+      if (!req.body.state) {
+          req.body.state = 'USA'
+      }
+
+      // Save the new data into places[id]
+      places[id] = req.body
+      res.redirect(`/places/${id}`)
+  }
+})
+
+// Delete Route
 router.delete('/:id', (req, res) => {
   let id = Number(req.params.id)
   if (isNaN(id)) {
     res.render('error404')
- }
- else if (!places[id]) {
-   res.render('error404')
- }
- else {
-   places.splice(id, 1)
-   res.redirect('/places')
- }
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    places.splice(id, 1)
+    res.redirect('/places')
+  }
 })
-
 module.exports = router
-
-
